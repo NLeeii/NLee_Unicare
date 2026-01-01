00aba1fc9f1890667c0a67f27f47f135fcc72be1
NLeeii <niniabc920405@gmail.com>
2026-01-01 23:14:43 +0800

    建立商城頁面(待處理scss變數)

---
+++ src/views/MallView.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="mall-page">
+    <h1>UniCare 樂活商城</h1>
+    <p>這裡是李妮負責的超強商城！</p>
+    <button class="test-btn">測試按鈕</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.mall-page {
+  padding: 20px;
+  
+  h1 {
+    // color: $primary-dark;
+  }
+
+  .test-btn {
+    // background-color: $orange;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 8px;
+  }
+}
+</style>
